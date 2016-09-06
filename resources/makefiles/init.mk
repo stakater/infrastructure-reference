b883309f4af864f4446a8d608b7826982305d928
Hazim <hazim@aurorasolutions.io>
2016-09-06 18:12:35 +0500

    Update init makefile to include common init functionality only

--- resources/makefiles/init.mk
+++ resources/makefiles/init.mk
@@ -1,35 +1,11 @@
-show: | $(BUILD)
-	cd $(BUILD); $(TF_SHOW)
-
-show_state: init
-	cat $(BUILD)/terraform.tfstate
-
-graph: | $(BUILD)
-	cd $(BUILD); $(TF_GRAPH)
-
-refresh: init
-	cd $(BUILD); $(TF_REFRESH)
-
-init: | $(TF_PROVIDER) $(MODULE_VARS)
-
 $(BUILD): init_build_dir
-
-$(TF_PROVIDER): update_provider
-
-$(MODULE_VARS): update_vars
 
 init_build_dir:
 	@rm -f $(BUILD)/*.tf
 	@mkdir -p $(BUILD)
-	@cp -rf $(RESOURCES)/cloud-config $(BUILD)
-	@cp -rf $(RESOURCES)/policies $(BUILD)
+	@mkdir -p $(BUILD_DEV)
+	@mkdir -p $(BUILD_PROD)
+	@mkdir -p $(BUILD_QA)
+	@mkdir -p $(BUILD_GLOBAL_ADMIRAL)
 
-update_vars:	| $(BUILD)
-	# Generate default AMI ids
-	$(SCRIPTS)/get-vars.sh > $(MODULE_VARS)
-
-update_provider: | $(BUILD)
-	# Generate tf provider
-	$(SCRIPTS)/gen-provider.sh > $(TF_PROVIDER)
-
-.PHONY: init show show_state graph refresh update_vars update_provider init_build_dir
+.PHONY: init_build_dir
