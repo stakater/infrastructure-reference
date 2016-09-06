c247ae5e1907498d8105c6e282bc552178710b9e
Hazim <hazim@aurorasolutions.io>
2016-09-06 18:14:40 +0500

    Update network make file to create all environment specific network modules

--- resources/makefiles/network.mk
+++ resources/makefiles/network.mk
@@ -1,55 +1,10 @@
-network: plan_network
-	cd $(BUILD); \
-	$(TF_APPLY) -target module.network.module.vpc \
-							-target module.network.module.private_persistence_subnet \
-							-target module.network.module.public_subnet \
-							-target module.network.module.nat \
-							-target module.network.module.network_acl \
-							-target module.network.module.private_app_subnet \
-							-target module.network;
-# Specifiy nested modules explicitly while using terraform apply, plan and destroy
-# https://github.com/hashicorp/terraform/issues/5870
+network: network_global_admiral network_dev network_qa network_prod
 
-plan_network: init_network
-	cd $(BUILD); \
-	$(TF_PLAN) -target module.network.module.vpc \
-	           -target module.network.module.private_persistence_subnet \
-						 -target module.network.module.public_subnet \
-						 -target module.network.module.nat \
-						 -target module.network.module.network_acl \
-						 -target module.network.module.private_app_subnet \
-						 -target module.network;
+plan_network: plan_network_global_admiral plan_network_dev plan_network_qa plan_network_prod
 
-refresh_network: | $(TF_PROVIDER)
-	cd $(BUILD); \
-	$(TF_REFRESH) -target module.network.module.vpc \
-								-target module.network.module.private_persistence_subnet \
-								-target module.network.module.public_subnet \
-								-target module.network.module.nat \
-								-target module.network.module.network_acl \
-								-target module.network.module.private_app_subnet \
-								-target module.network;
+refresh_network: refresh_network_global_admiral refresh_network_dev refresh_network_qa refresh_network_prod
 
-destroy_network: | $(TF_PROVIDER)
-	cd $(BUILD); \
-	$(TF_DESTROY) -target module.network \
-								-target module.network.module.network_acl \
-								-target module.network.module.private_app_subnet \
-								-target module.network.module.nat \
-								-target module.network.module.public_subnet \
-								-target module.network.module.private_persistence_subnet \
-								-target module.network.module.vpc;
-# Bug: While using destroy, it asks for aws region as input from the user
-# The region value from provider resource is not passed on to the submodules.
-# https://github.com/hashicorp/terraform/issues/1447 (Issue resurfaced after upgrade to TF 0.7)
-# https://github.com/hashicorp/terraform/issues/3081
+destroy_network: destroy_network_qa destroy_network_dev destroy_network_global_admiral
+	@echo -e "\033[0;31m \n\nNOTE:\nnetwork_prod is not destroyed through this command for safety, please destroy it through 'destroy_network_prod' if required \033[0m"
 
-clean_network: destroy_network
-	rm -f $(BUILD)/module-network.tf
-
-init_network: init
-	cp -rf $(RESOURCES)/terraforms/module-network.tf $(BUILD)
-	cd $(BUILD); $(TF_GET);
-
-.PHONY: network destroy_network refresh_network plan_network init_network clean_network
-
+.PHONY: network destroy_network refresh_network plan_network
