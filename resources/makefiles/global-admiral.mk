a165ec9fc571725f58c002a093fd0adf2e68d000
Hazim <hazim@aurorasolutions.io>
2016-09-20 20:27:21 +0500

    Add environment make files

---
+++ resources/makefiles/global-admiral.mk
@@ -0,0 +1,9 @@
+global_admiral: storage_global_admiral network_global_admiral
+
+plan_global_admiral: plan_storage_global_admiral plan_network_global_admiral
+
+refresh_global_admiral: refresh_storage_global_admiral refresh_network_global_admiral
+
+destroy_global_admiral: destroy_storage_global_admiral destroy_network_global_admiral
+
+.PHONY: global_admiral destroy_global_admiral refresh_global_admiral plan_global_admiral
---
+++ resources/makefiles/qa.mk
@@ -0,0 +1,9 @@
+qa: storage_qa network_qa
+
+plan_qa: plan_storage_qa plan_network_qa
+
+refresh_qa: refresh_storage_qa refresh_network_qa
+
+destroy_qa: destroy_storage_qa destroy_network_qa
+
+.PHONY: qa destroy_qa refresh_qa plan_qa
