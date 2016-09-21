9b6bf2624cd18da2e3134c6115403dae21e3c36c
Hazim <hazim@aurorasolutions.io>
2016-09-21 10:59:10 +0500

    Rearrange destroy sequence

--- resources/makefiles/dev.mk
+++ resources/makefiles/dev.mk
@@ -4,6 +4,6 @@ plan_dev: plan_storage_dev plan_network_dev
 
 refresh_dev: refresh_storage_dev refresh_network_dev
 
-destroy_dev: destroy_storage_dev destroy_network_dev
+destroy_dev: destroy_network_dev destroy_storage_dev
 
 .PHONY: dev destroy_dev refresh_dev plan_dev
--- resources/makefiles/prod.mk
+++ resources/makefiles/prod.mk
@@ -4,6 +4,6 @@ plan_prod: plan_storage_prod plan_network_prod
 
 refresh_prod: refresh_storage_prod refresh_network_prod
 
-destroy_prod: destroy_storage_prod destroy_network_prod
+destroy_prod: destroy_network_prod destroy_storage_prod
 
 .PHONY: prod destroy_prod refresh_prod plan_prod
