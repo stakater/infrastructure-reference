a1f9b45ffdd51fd10efa510af5094a2c59f34b0b
Hazim <hazim@aurorasolutions.io>
2016-09-27 20:39:04 +0500

    Add Cloudiniti bucket

--- infrastructure-modules/global-admiral/storage/storage.tf
+++ infrastructure-modules/global-admiral/storage/storage.tf
@@ -2,3 +2,8 @@ module "config-bucket" {
   source = "github.com/stakater/blueprint-storage-aws.git//modules/s3"
   name = "${var.stack_name}-global-admiral-config"
 }
+
+module "cloudinit-bucket" {
+  source = "github.com/stakater/blueprint-storage-aws.git//modules/s3"
+  name = "${var.stack_name}-global-admiral-cloudinit"
+}
--- resources/makefiles/global-admiral/storage-global-admiral.mk
+++ resources/makefiles/global-admiral/storage-global-admiral.mk
@@ -1,20 +1,24 @@
 storage_global_admiral: plan_storage_global_admiral
 	cd $(BUILD_GLOBAL_ADMIRAL); \
-	$(TF_APPLY)	-target module.config-bucket;
+	$(TF_APPLY)	-target module.config-bucket \
+							-target module.cloudinit-bucket;
 # Specifiy nested modules explicitly while using terraform apply, plan and destroy
 # https://github.com/hashicorp/terraform/issues/5870
 
 plan_storage_global_admiral: init_storage_global_admiral
 	cd $(BUILD_GLOBAL_ADMIRAL); \
-	$(TF_PLAN) -target module.config-bucket;
+	$(TF_PLAN) -target module.config-bucket \
+						 -target module.cloudinit-bucket;
 
 refresh_storage_global_admiral: | $(TF_PROVIDER_GLOBAL_ADMIRAL) pull_global_admiral_state
 	cd $(BUILD_GLOBAL_ADMIRAL); \
-	$(TF_REFRESH) -target module.config-bucket;
+	$(TF_REFRESH) -target module.config-bucket \
+								-target module.cloudinit-bucket;
 
 destroy_storage_global_admiral: | $(TF_PROVIDER_GLOBAL_ADMIRAL) pull_global_admiral_state
 	cd $(BUILD_GLOBAL_ADMIRAL); \
-	$(TF_DESTROY) -target module.config-bucket;
+	$(TF_DESTROY) -target module.cloudinit-bucket \
+								-target module.config-bucket;
 
 clean_storage_global_admiral: destroy_storage_global_admiral
 	rm -f $(BUILD_GLOBAL_ADMIRAL)/storage.tf
