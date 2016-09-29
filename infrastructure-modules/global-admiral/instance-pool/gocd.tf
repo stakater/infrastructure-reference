65067c85917ddc4fc94bd9c0b2299501449194f4
Hazim <hazim@aurorasolutions.io>
2016-09-29 17:00:57 +0500

    Update bucket source path and gocd volume sizes

--- infrastructure-modules/global-admiral/instance-pool/gocd.tf
+++ infrastructure-modules/global-admiral/instance-pool/gocd.tf
@@ -18,11 +18,11 @@ module "gocd" {
   iam_role_policy  = "${data.template_file.gocd-policy.rendered}"
   user_data        = "${data.template_file.gocd-bootstrap-user-data.rendered}"
   key_name         = "gocd"
-  root_vol_size    = 20
+  root_vol_size    = 50
   data_ebs_device_name  = "/dev/sdf"
-  data_ebs_vol_size     = 12
+  data_ebs_vol_size     = 100
   logs_ebs_device_name  = "/dev/sdg"
-  logs_ebs_vol_size     = 12
+  logs_ebs_vol_size     = 20
 
   # ASG parameters
   max_size         = "1"
@@ -76,62 +76,62 @@ resource "aws_s3_bucket_object" "gocd_cloud_config" {
 resource "aws_s3_bucket_object" "gocd_build_ami" {
   bucket = "${module.config-bucket.bucket_name}"
   key = "gocd/scripts/build-ami.sh"
-  source = "./gocd-data/scripts/build-ami.sh"
+  source = "./data/gocd/scripts/build-ami.sh"
 }
 resource "aws_s3_bucket_object" "gocd_build_docker_image" {
   bucket = "${module.config-bucket.bucket_name}"
   key = "gocd/scripts/build-docker-image.sh"
-  source = "./gocd-data/scripts/build-docker-image.sh"
+  source = "./data/gocd/scripts/build-docker-image.sh"
 }
 resource "aws_s3_bucket_object" "gocd_compile_code" {
   bucket = "${module.config-bucket.bucket_name}"
   key = "gocd/scripts/compile-code.sh"
-  source = "./gocd-data/scripts/compile-code.sh"
+  source = "./data/gocd/scripts/compile-code.sh"
 }
 resource "aws_s3_bucket_object" "gocd_deploy_to_cluster" {
   bucket = "${module.config-bucket.bucket_name}"
   key = "gocd/scripts/deploy-to-cluster.sh"
-  source = "./gocd-data/scripts/deploy-to-cluster.sh"
+  source = "./data/gocd/scripts/deploy-to-cluster.sh"
 }
 resource "aws_s3_bucket_object" "gocd_docker_cleanup" {
   bucket = "${module.config-bucket.bucket_name}"
   key = "gocd/scripts/docker-cleanup.sh"
-  source = "./gocd-data/scripts/docker-cleanup.sh"
+  source = "./data/gocd/scripts/docker-cleanup.sh"
 }
 resource "aws_s3_bucket_object" "gocd_gocd_parameters" {
   bucket = "${module.config-bucket.bucket_name}"
   key = "gocd/scripts/gocd.parameters.txt"
-  source = "./gocd-data/scripts/gocd.parameters.txt"
+  source = "./data/gocd/scripts/gocd.parameters.txt"
 }
 resource "aws_s3_bucket_object" "gocd_launch_ami" {
   bucket = "${module.config-bucket.bucket_name}"
   key = "gocd/scripts/launch-ami.sh"
-  source = "./gocd-data/scripts/launch-ami.sh"
+  source = "./data/gocd/scripts/launch-ami.sh"
 }
 resource "aws_s3_bucket_object" "gocd_read_parameter" {
   bucket = "${module.config-bucket.bucket_name}"
   key = "gocd/scripts/read-parameters.sh"
-  source = "./gocd-data/scripts/read-parameter.sh"
+  source = "./data/gocd/scripts/read-parameter.sh"
 }
 resource "aws_s3_bucket_object" "gocd_test" {
   bucket = "${module.config-bucket.bucket_name}"
   key = "gocd/scripts/test-code.sh"
-  source = "./gocd-data/scripts/test-code.sh"
+  source = "./data/gocd/scripts/test-code.sh"
 }
 resource "aws_s3_bucket_object" "gocd_cruise_config" {
   bucket = "${module.config-bucket.bucket_name}"
   key = "gocd/conf/cruise-config.xml"
-  source = "./gocd-data/conf/cruise-config.xml"
+  source = "./data/gocd/conf/cruise-config.xml"
 }
 resource "aws_s3_bucket_object" "gocd_passwd" {
   bucket = "${module.config-bucket.bucket_name}"
   key = "gocd/conf/passwd"
-  source = "./gocd-data/conf/passwd"
+  source = "./data/gocd/conf/passwd"
 }
 resource "aws_s3_bucket_object" "gocd_sudoers" {
   bucket = "${module.config-bucket.bucket_name}"
   key = "gocd/conf/sudoers"
-  source = "./gocd-data/conf/sudoers"
+  source = "./data/gocd/conf/sudoers"
 }
 ####################################################
 
