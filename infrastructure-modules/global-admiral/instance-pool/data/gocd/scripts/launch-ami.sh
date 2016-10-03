1da44b1624827367a3e12e302c25d1183914eb01
HasnainASyed <hasnain.a.syed@gmail.com>
2016-10-03 12:16:52 +0500

    Update gitignore and separate build ami and launch ami pipelines

--- infrastructure-modules/global-admiral/instance-pool/data/gocd/scripts/launch-ami.sh
+++ infrastructure-modules/global-admiral/instance-pool/data/gocd/scripts/launch-ami.sh
@@ -1,28 +1,76 @@
 #!/bin/bash
 # THis shell script deploys and AMI 
 #-----------------------------------------------------
-# Argument1: AMI_ID
-# Argument2: VPC_ID
-# Argument3: SUBNET_ID
-# Argument4: AWS_REGION
+# Argument1: APP_NAME
 #-----------------------------------------------------
 
 # Get parameter values
-AMI_ID=$1
-VPC_ID=$2
-SUBNET_ID=$3
-AWS_REGION=$4
+APP_NAME=$1
+
+# Check number of parameters equal 1
+if [ "$#" -ne 1 ]; then
+    echo "ERROR: [Launch AMI] Illegal number of parameters"
+    exit 1
+fi
+
+ARE_AMI_PARAMS_EMPTY=false;
+AMI_PARAMS_FILE="/app/${APP_NAME}/cd/vars/${APP_NAME}_ami_params.txt"
+
+# Check ami params file exist
+if [ ! -f ${AMI_PARAMS_FILE} ];
+then
+   echo "Error: [Launch AMI] AMI parameters file not found";
+   exit 1;
+fi;
+
+# Read parameter values from file
+AMI_ID=`/gocd-data/scripts/read-parameter.sh ${AMI_PARAMS_FILE} AMI_ID`
+VPC_ID=`/gocd-data/scripts/read-parameter.sh ${AMI_PARAMS_FILE} VPC_ID`
+SUBNET_ID=`/gocd-data/scripts/read-parameter.sh ${AMI_PARAMS_FILE} SUBNET_ID`
+AWS_REGION=`/gocd-data/scripts/read-parameter.sh ${AMI_PARAMS_FILE} AWS_REGION`
 
 # Output values
 echo "###################################################"
+echo "APP_NAME: ${APP_NAME}"
 echo "AMI_ID: ${AMI_ID}"
 echo "VPC_ID: ${VPC_ID}"
 echo "SUBNET_ID: ${SUBNET_ID}"
 echo "AWS_REGION: ${AWS_REGION}"
 echo "###################################################"
 
-## Automated Deployment
+# Check parameter values not empty
+if test -z ${AMI_ID};
+then
+   echo "Error: Value for AMI ID not defined.";
+   ARE_AMI_PARAMS_EMPTY=true;
+fi;
 
+if test -z ${VPC_ID};
+then
+   echo "Error: Value for VPC ID not defined.";
+   ARE_AMI_PARAMS_EMPTY=true;
+fi;
+
+if test -z ${SUBNET_ID};
+then
+   echo "Error: Value for SUBNET ID not defined.";
+   ARE_AMI_PARAMS_EMPTY=true;
+fi;
+
+if test -z ${AWS_REGION};
+then
+   echo "Error: Value for AWS REGION not defined.";
+   ARE_AMI_PARAMS_EMPTY=true;
+fi;
+
+# Check ami params not empty
+if $ARE_AMI_PARAMS_EMPTY;
+then
+    echo "ERROR: Invalid AMI parameters.";
+    exit 1;
+fi;
+
+## Automated Deployment
 # Download amd update modules
 cd ~/terraform-aws-asg/examples/standard/;
 sudo /opt/terraform/terraform get -update .;
