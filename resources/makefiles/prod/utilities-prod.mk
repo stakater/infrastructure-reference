4b353aec2718ca63c844d93eee3c60e507b810dd
Hazim <hazim@aurorasolutions.io>
2016-10-05 15:09:29 +0500

    Add Route53 module

--- resources/makefiles/prod/utilities-prod.mk
+++ resources/makefiles/prod/utilities-prod.mk
@@ -1,14 +1,22 @@
 utilities_prod: plan_utilities_prod
-	cd $(BUILD_PROD);
+	cd $(BUILD_PROD); \
+	$(TF_APPLY)	-target module.route53-private \
+							-target aws_route53_record.docker-registry;
 
 plan_utilities_prod: init_utilities_prod
-	cd $(BUILD_PROD);
+	cd $(BUILD_PROD); \
+	$(TF_PLAN) -target module.route53-private \
+						 -target aws_route53_record.docker-registry;
 
 refresh_utilities_prod: | $(TF_PROVIDER_PROD) pull_prod_state
-	cd $(BUILD_PROD);
+	cd $(BUILD_PROD);\
+	$(TF_REFRESH) -target module.route53-private \
+								-target aws_route53_record.docker-registry;
 
 destroy_utilities_prod: | $(TF_PROVIDER_PROD) pull_prod_state
-	cd $(BUILD_PROD);
+	cd $(BUILD_PROD);\
+	$(TF_DESTROY) -target aws_route53_record.docker-registry \
+								-target module.route53-private;
 
 init_utilities_prod: init_prod
 		cp -rf $(INFRA_PROD)/utilities/*.tf $(BUILD_PROD)
--- resources/makefiles/qa/utilities-qa.mk
+++ resources/makefiles/qa/utilities-qa.mk
@@ -1,14 +1,24 @@
 utilities_qa: plan_utilities_qa
-	cd $(BUILD_QA);
+	cd $(BUILD_QA); \
+	$(TF_APPLY)	-target module.route53-private \
+							-target aws_route53_record.docker-registry;
 
 plan_utilities_qa: init_utilities_qa
-	cd $(BUILD_QA);
+	cd $(BUILD_QA); \
+	$(TF_PLAN) -target module.route53-private \
+						 -target aws_route53_record.docker-registry;
 
 refresh_utilities_qa: | $(TF_PROVIDER_QA) pull_qa_state
-	cd $(BUILD_QA);
+	cd $(BUILD_QA); \
+	$(TF_REFRESH) -target module.route53-private \
+								-target aws_route53_record.docker-registry;
+
 
 destroy_utilities_qa: | $(TF_PROVIDER_QA) pull_qa_state
-	cd $(BUILD_QA);
+	cd $(BUILD_QA);\
+	$(TF_DESTROY) -target aws_route53_record.docker-registry \
+								-target module.route53-private;
+
 
 init_utilities_qa: init_qa
 		cp -rf $(INFRA_QA)/utilities/*.tf $(BUILD_QA)
