53562ec2c762540fa81a49abe8711475afe54bcc
Hazim <hazim@aurorasolutions.io>
2016-10-21 20:43:41 +0500

    Fix admiral build location variable

--- resources/makefiles/global-admiral.mk
+++ resources/makefiles/global-admiral.mk
@@ -6,7 +6,7 @@ global_admiral: storage_global_admiral network_global_admiral utilities_global_a
 plan_global_admiral: plan_storage_global_admiral plan_network_global_admiral plan_utilities_global_admiral plan_instance_pool_global_admiral
 
 refresh_global_admiral: init_global_admiral
-	cd $(BUILD); $(TF_REFRESH)
+	cd $(BUILD_GLOBAL_ADMIRAL); $(TF_REFRESH)
 
 destroy_global_admiral: destroy_instance_pool_global_admiral destroy_utilities_global_admiral destroy_network_global_admiral destroy_storage_global_admiral
 
