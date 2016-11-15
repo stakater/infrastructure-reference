1a29302c3832648992656177740b01e8fb97ccbf
Hazim <hazim@aurorasolutions.io>
2016-11-15 17:47:50 +0500

    Add clean_all

--- resources/makefiles/all.mk
+++ resources/makefiles/all.mk
@@ -6,4 +6,8 @@ refresh_all: refresh_global_admiral refresh_dev refresh_qa refresh_prod
 
 destroy_all: destroy_prod destroy_qa destroy_dev destroy_global_admiral
 
+clean_all: destroy_all
+	rm -f $(BUILD)/*
+	rm -rf $(BUILD)/.terraform
+
 .PHONY: all destroy_all refresh_all plan_all
