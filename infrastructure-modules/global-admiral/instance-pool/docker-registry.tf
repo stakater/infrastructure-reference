7aad388c4a25a9a75f3dbd88ac1d47f6cc6d13bf
Hazim <hazim@aurorasolutions.io>
2016-11-18 12:28:21 +0500

    Change instance type to t2.nano

--- infrastructure-modules/global-admiral/instance-pool/docker-registry.tf
+++ infrastructure-modules/global-admiral/instance-pool/docker-registry.tf
@@ -12,7 +12,7 @@ module "docker-registry" {
 
   # LC parameters
   ami              = "${var.ami}"
-  instance_type    = "t2.small"
+  instance_type    = "t2.nano"
   iam_assume_role_policy = "${file("./policy/assume-role-policy.json")}"
   iam_role_policy  = "${data.template_file.docker-registry-policy.rendered}"
   user_data        = "${data.template_file.docker-registry-bootstrap-user-data.rendered}"
