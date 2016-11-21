6ac53d3868d316b870c64451a94a857be6d4f6c2
Muhammad Hamza Zaib <hamza@aurorasolutions.io>
2016-11-21 16:47:44 +0500

    add maintenance page bucket and index page to prod environment

--- infrastructure-modules/prod/storage/storage.tf
+++ infrastructure-modules/prod/storage/storage.tf
@@ -8,6 +8,37 @@ module "cloudinit-bucket" {
   name = "${var.prod_cloudinit_bucket_name}"
 }
 
+module "maintenance-page-bucket" {
+  source = "github.com/stakater/blueprint-storage-aws.git//modules/s3-static-website-hosting?ref=static-website-hosting"
+  name = "${var.stack_name}-maintenance-page}"
+  policy = "${data.template_file.static-website-hosting-policy.rendered}"
+  index = "index.html"
+  error = "error.html"
+}
+
+##########################
+# Upload Index file to s3
+##########################
+resource "aws_s3_bucket_object" "upload-maintenance-index-page" {
+  bucket = "${module.maintenance-page-bucket.bucket_name}"
+  key = "index.html"
+  content = "${file("./data/index.html")}"
+}
+resource "aws_s3_bucket_object" "upload-maintenance-error-page" {
+  bucket = "${module.maintenance-page-bucket.bucket_name}"
+  key = "error.html"
+  content = "${file("./data/error.html")}"
+}
+
+
+## Template files
+data "template_file" "static-website-hosting-policy" {
+  template = "${file("./policy/static-website-hosting-policy.json")}"
+  vars {
+    bucket_name = "${var.stack_name}-maintenance-page}"
+  }
+}
+
 # Outputs to be accessible through remote state
 output "config-bucket-arn" {
   value = "${module.config-bucket.arn}"
@@ -19,4 +50,16 @@ output "config-bucket-name" {
 
 output "cloudinit-bucket-arn" {
   value = "${module.cloudinit-bucket.arn}"
+}
+
+output "maintenance-page-bucket-arn" {
+  value = "${module.maintenance-page-bucket.arn}"
+}
+
+output "maintenance-page-bucket-name" {
+  value = "${module.maintenance-page-bucket.bucket_name}"
+}
+
+output "maintenance-page-website-endpoint" {
+  value = "${module.maintenance-page-bucket.website_endpoint}"
 }
--- resources/makefiles/prod/storage-prod.mk
+++ resources/makefiles/prod/storage-prod.mk
@@ -1,24 +1,36 @@
 storage_prod: plan_storage_prod
 	cd $(BUILD_PROD); \
 	$(TF_APPLY)	-target module.config-bucket \
-							-target module.cloudinit-bucket;
+                -target module.cloudinit-bucket \
+                -target module.maintenance-page-bucket \
+                -target aws_s3_bucket_object.upload-maintenance-index-page \
+                -target aws_s3_bucket_object.upload-maintenance-error-page;
 # Specifiy nested modules explicitly while using terraform apply, plan and destroy
 # https://github.com/hashicorp/terraform/issues/5870
 
 plan_storage_prod: init_storage_prod
 	cd $(BUILD_PROD); \
-	$(TF_PLAN) -target module.config-bucket \
-						 -target module.cloudinit-bucket;
+	$(TF_PLAN)  -target module.config-bucket \
+			    -target module.cloudinit-bucket \
+			    -target module.maintenance-page-bucket \
+                -target aws_s3_bucket_object.upload-maintenance-index-page \
+                -target aws_s3_bucket_object.upload-maintenance-error-page;
 
 refresh_storage_prod: | $(TF_PROVIDER_PROD) pull_prod_state
 	cd $(BUILD_PROD); \
 	$(TF_REFRESH) -target module.config-bucket \
-								-target module.cloudinit-bucket;
+                  -target module.cloudinit-bucket \
+                  -target module.maintenance-page-bucket \
+                  -target aws_s3_bucket_object.upload-maintenance-index-page \
+                  -target aws_s3_bucket_object.upload-maintenance-error-page;
 
 destroy_storage_prod: | $(TF_PROVIDER_PROD) pull_prod_state
 	cd $(BUILD_PROD); \
 	$(TF_DESTROY) -target module.config-bucket \
-								-target module.cloudinit-bucket;
+                  -target module.cloudinit-bucket \
+                  -target aws_s3_bucket_object.upload-maintenance-index-page \
+                  -target aws_s3_bucket_object.upload-maintenance-error-page \
+                  -target module.maintenance-page-bucket;
 
 clean_storage_prod: destroy_storage_prod
 	rm -f $(BUILD_PROD)/storage.tf
