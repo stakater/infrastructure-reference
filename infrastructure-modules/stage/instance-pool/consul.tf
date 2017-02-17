efd43e32ff4ed92de26f6b5a95bded8e27ed4507
hazim1093 <hazim_malik@hotmail.com>
2017-02-17 17:36:04 +0500

    Add consul resources

---
+++ infrastructure-modules/stage/instance-pool/consul.tf
@@ -0,0 +1,389 @@
+###############################################################################
+# Copyright 2016 Aurora Solutions
+#
+#    http://www.aurorasolutions.io
+#
+# Aurora Solutions is an innovative services and product company at
+# the forefront of the software industry, with processes and practices
+# involving Domain Driven Design(DDD), Agile methodologies to build
+# scalable, secure, reliable and high performance products.
+#
+# Stakater is an Infrastructure-as-a-Code DevOps solution to automate the
+# creation of web infrastructure stack on Amazon. Stakater is a collection
+# of Blueprints; where each blueprint is an opinionated, reusable, tested,
+# supported, documented, configurable, best-practices definition of a piece
+# of infrastructure. Stakater is based on Docker, CoreOS, Terraform, Packer,
+# Docker Compose, GoCD, Fleet, ETCD, and much more.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+## Provisions basic autoscaling group
+module "consul" {
+  source = "git::https://github.com/stakater/blueprint-instance-pool-aws.git//modules/instance-pool"
+
+  # Resource tags
+  name = "${var.stack_name}-stage-consul"
+
+  # VPC parameters
+  vpc_id  = "${module.network.vpc_id}"
+  subnets = "${module.network.private_app_subnet_ids}"
+  region  = "${var.aws_account["default_region"]}"
+
+  # LC parameters
+  ami              = "${var.ami}"
+  instance_type    = "t2.nano"
+  iam_assume_role_policy = "${file("./policy/assume-role-policy.json")}"
+  iam_role_policy  = "${data.template_file.consul-policy.rendered}"
+  user_data        = "${data.template_file.consul-bootstrap-user-data.rendered}"
+  key_name         = "consul-stage"
+  root_vol_size    = 20
+  root_vol_del_on_term = true
+  data_ebs_device_name  = ""
+  data_ebs_vol_size     = 0
+  logs_ebs_device_name  = ""
+  logs_ebs_vol_size     = 0
+
+  # ASG parameters
+  max_size         = "1"
+  min_size         = "1"
+  desired_size     = "1"
+  min_elb_capacity = "1"
+  load_balancers   = "${aws_elb.consul-elb.id}"
+}
+
+## Template files
+data "template_file" "consul-policy" {
+  template = "${file("./policy/consul-role-policy.json")}"
+
+  vars {
+    config_bucket_arn = "${module.config-bucket.arn}"
+    cloudinit_bucket_arn = "${module.cloudinit-bucket.arn}"
+    global_admiral_config_bucket_arn = "${data.terraform_remote_state.global-admiral.config-bucket-arn}"
+  }
+}
+
+data "template_file" "consul-bootstrap-user-data" {
+  template = "${file("./user-data/bootstrap-user-data.sh.tmpl")}"
+
+  vars {
+    stack_name = "${var.stack_name}"
+    config_bucket_name = "${data.terraform_remote_state.global-admiral.config-bucket-name}"
+    cloudinit_bucket_name = "${module.cloudinit-bucket.bucket_name}"
+    module_name = "consul"
+    additional_user_data_scripts = ""
+  }
+}
+
+data "template_file" "consul-user-data" {
+  template = "${file("./user-data/consul.yaml")}"
+
+  vars {
+    stack_name = "${var.stack_name}"
+    config_bucket_name = "${module.config-bucket.bucket_name}"
+    global_admiral_config_bucket="${data.terraform_remote_state.global-admiral.config-bucket-name}"
+  }
+}
+
+# Upload CoreOS cloud-config to a s3 bucket; bootstrap-user-data.sh script in user-data will download
+# the cloud-config upon reboot to configure the system. This avoids rebuilding machines when
+# changing cloud-config.
+resource "aws_s3_bucket_object" "consul_cloud_config" {
+  bucket = "${module.cloudinit-bucket.bucket_name}"
+  key = "consul/cloud-config.yaml"
+  content = "${data.template_file.consul-user-data.rendered}"
+}
+
+## Creates ELB security group
+resource "aws_security_group" "consul-sg-elb" {
+  name_prefix = "${var.stack_name}-stage-elb-"
+  vpc_id      = "${module.network.vpc_id}"
+
+  tags {
+    Name        = "${var.stack_name}-elb"
+    managed_by  = "Stakater"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  # Allow HTTP traffic
+  ingress {
+    cidr_blocks = ["${module.network.vpc_cidr}"]
+    from_port   = 8300
+    to_port     = 8301
+    protocol    = "tcp"
+  }
+  # Allow HTTP traffic
+  ingress {
+    cidr_blocks = ["${module.network.vpc_cidr}"]
+    from_port   = 8400
+    to_port     = 8400
+    protocol    = "tcp"
+  }
+
+  # Allow HTTP traffic
+  ingress {
+    cidr_blocks = ["${module.network.vpc_cidr}"]
+    from_port   = 8500
+    to_port     = 8500
+    protocol    = "tcp"
+  }
+
+  # Allow HTTP traffic
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
+}
+
+## Creates ELB
+resource "aws_elb" "consul-elb" {
+  name                      = "${var.stack_name}-stage-consul"
+  security_groups           = ["${aws_security_group.consul-sg-elb.id}"]
+  subnets                   = ["${split(",",module.network.public_subnet_ids)}"]
+  internal                  = false
+  cross_zone_load_balancing = true
+  connection_draining       = true
+
+  tags {
+    Name        = "${var.stack_name}-stage-consul"
+    managed_by  = "Stakater"
+  }
+
+  listener {
+    instance_port     = 8300
+    instance_protocol = "http"
+    lb_port           = 8300
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port     = 8301
+    instance_protocol = "http"
+    lb_port           = 8301
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port     = 8400
+    instance_protocol = "http"
+    lb_port           = 8400
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port     = 8500
+    instance_protocol = "http"
+    lb_port           = 8500
+    lb_protocol       = "http"
+  }
+
+  # Redirect standard http port to consul UI port
+  listener {
+    instance_port     = 8500
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 10
+    unhealthy_threshold = 2
+    timeout             = 5
+    target              = "TCP:22"
+    interval            = 30
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Route53 record
+# Add to stage's private dns
+resource "aws_route53_record" "consul" {
+  zone_id = "${module.route53-private.zone_id}"
+  name = "consul-stage"
+  type = "A"
+
+  alias {
+    name = "${aws_elb.consul-elb.dns_name}"
+    zone_id = "${aws_elb.consul-elb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+##############################
+## Security Group Rules
+##############################
+# Allow ssh from within vpc
+resource "aws_security_group_rule" "sg-consul-ssh" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  cidr_blocks              = ["${module.network.vpc_cidr}"]
+  security_group_id        = "${module.consul.security_group_id}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Allow Outgoing traffic
+resource "aws_security_group_rule" "sg-consul-outgoing" {
+  type                     = "egress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  cidr_blocks              = ["0.0.0.0/0"]
+  security_group_id        = "${module.consul.security_group_id}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_security_group_rule" "sg-consul-8300" {
+  type                     = "ingress"
+  from_port                = 8300
+  to_port                  = 8300
+  protocol                 = "tcp"
+  cidr_blocks              = ["${module.network.vpc_cidr}"]
+  security_group_id        = "${module.consul.security_group_id}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "sg-consul-8301" {
+  type                     = "ingress"
+  from_port                = 8301
+  to_port                  = 8301
+  protocol                 = "tcp"
+  cidr_blocks              = ["${module.network.vpc_cidr}"]
+  security_group_id        = "${module.consul.security_group_id}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "sg-consul-8400" {
+  type                     = "ingress"
+  from_port                = 8400
+  to_port                  = 8400
+  protocol                 = "tcp"
+  cidr_blocks              = ["${module.network.vpc_cidr}"]
+  security_group_id        = "${module.consul.security_group_id}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "sg-consul-8500" {
+  type                     = "ingress"
+  from_port                = 8500
+  to_port                  = 8500
+  protocol                 = "tcp"
+  cidr_blocks              = ["${module.network.vpc_cidr}"]
+  security_group_id        = "${module.consul.security_group_id}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+## Allow access to etcd
+resource "aws_security_group_rule" "sg-consul-etcd" {
+  type                     = "ingress"
+  from_port                = 2379
+  to_port                  = 2380
+  protocol                 = "tcp"
+  cidr_blocks              = ["${module.network.vpc_cidr}"]
+  security_group_id        = "${data.terraform_remote_state.global-admiral.etcd-security-group-id}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+####################################
+# ASG Scaling Policies
+####################################
+## Provisions autoscaling policies and associated resources
+module "consul_scale_up_policy" {
+  source = "git::https://github.com/stakater/blueprint-instance-pool-aws.git//modules/asg-policy"
+
+  # Resource tags
+  name = "${var.stack_name}-stage-consul-scaleup-policy"
+
+  # ASG parameters
+  asg_name = "${module.consul.asg_name}"
+  asg_id   = "${module.consul.asg_id}"
+
+  # Notification parameters
+  notifications = "autoscaling:EC2_INSTANCE_LAUNCH_ERROR,autoscaling:EC2_INSTANCE_TERMINATE_ERROR"
+
+  # Monitor parameters
+  adjustment_type          = "PercentChangeInCapacity"
+  scaling_adjustment       = 30
+  cooldown                 = 300
+  min_adjustment_magnitude = 2
+  comparison_operator      = "GreaterThanOrEqualToThreshold"
+  evaluation_periods       = 2
+  metric_name              = "CPUUtilization"
+  period                   = 120
+  threshold                = 10
+}
+
+module "consul_scale_down_policy" {
+  source = "git::https://github.com/stakater/blueprint-instance-pool-aws.git//modules/asg-policy"
+
+  # Resource tags
+  name = "${var.stack_name}-stage-consul-scaledown-policy"
+
+  # ASG parameters
+  asg_name = "${module.consul.asg_name}"
+  asg_id   = "${module.consul.asg_id}"
+
+  # Notification parameters
+  notifications = "autoscaling:EC2_INSTANCE_LAUNCH_ERROR,autoscaling:EC2_INSTANCE_TERMINATE_ERROR"
+
+  # Monitor parameters
+  adjustment_type     = "ChangeInCapacity"
+  scaling_adjustment  = -1
+  cooldown            = 300
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = 30
+  metric_name         = "CPUUtilization"
+  period              = 60
+  threshold           = 50
+}
+
+# Outputs to be accessible through remote state
+output "consul-security-group-id" {
+  value = "${module.consul.security_group_id}"
+}
