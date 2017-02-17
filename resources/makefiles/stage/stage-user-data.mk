fc13b0b5ee23943a893e1a28d11ad57a2b51086f
hazim1093 <hazim_malik@hotmail.com>
2017-02-17 17:38:32 +0500

    Update makefiles

--- resources/makefiles/stage/stage-user-data.mk
+++ resources/makefiles/stage/stage-user-data.mk
@@ -1,25 +1,34 @@
 stage_user_data: plan_stage_user_data
 	cd $(BUILD_STAGE); \
-	$(TF_APPLY) -target aws_s3_bucket_object.stage-cloud-config;
+	$(TF_APPLY) -target aws_s3_bucket_object.stage-cloud-config \
+				-target aws_s3_bucket_object.stage-filebeat-config-tmpl \
+				-target aws_s3_bucket_object.admiral-cloud-config;
 
 plan_stage_user_data: init_stage_user_data
 	cd $(BUILD_STAGE); \
-	$(TF_PLAN) -target aws_s3_bucket_object.stage-cloud-config;
+	$(TF_PLAN) -target aws_s3_bucket_object.stage-cloud-config \
+			   -target aws_s3_bucket_object.stage-filebeat-config-tmpl \
+			   -target aws_s3_bucket_object.admiral-cloud-config;
 
 refresh_stage_user_data: | $(TF_PROVIDER_STAGE) pull_stage_state
 	cd $(BUILD_STAGE); \
-	$(TF_REFRESH) -target aws_s3_bucket_object.stage-cloud-config;
+	$(TF_REFRESH) -target aws_s3_bucket_object.stage-cloud-config \
+				  -target aws_s3_bucket_object.stage-filebeat-config-tmpl \
+                  -target aws_s3_bucket_object.admiral-cloud-config;
 
 destroy_stage_user_data: | $(TF_PROVIDER_STAGE) pull_stage_state
 	cd $(BUILD_STAGE); \
-	$(TF_DESTROY)	-target aws_s3_bucket_object.stage-cloud-config;
+	$(TF_DESTROY)	-target aws_s3_bucket_object.stage-cloud-config \
+					-target aws_s3_bucket_object.stage-filebeat-config-tmpl \
+                    -target aws_s3_bucket_object.admiral-cloud-config;
 
 clean_stage_user_data: destroy_stage_user_data
 	rm -f $(BUILD_STAGE)/stage-user-data.tf
 
 init_stage_user_data: init_stage
+		cp -rf $(INFRA_STAGE)/instance-pool/data/worker/ $(BUILD_STAGE)/data;
 		cp -rf $(INFRA_STAGE)/instance-pool/stage-user-data.tf $(BUILD_STAGE);
-		cp -rf $(INFRA_STAGE)/instance-pool/user-data/stage* $(BUILD_STAGE)/user-data;
+		cp -rf $(INFRA_STAGE)/instance-pool/user-data/* $(BUILD_STAGE)/user-data;
 		cd $(BUILD_STAGE); $(TF_GET);
 
 .PHONY: stage_user_data destroy_stage_user_data refresh_stage_user_data plan_stage_user_data init_stage_user_data clean_stage_user_data
